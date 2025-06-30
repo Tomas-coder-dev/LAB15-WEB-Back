54142beb6f25b494e4385329d738e6cda9ecd223
Fabricio AM <149453943+Tomas-coder-dev@users.noreply.github.com>
2025-06-30 12:02:26 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize');
-const db = new Sequelize('bd_farmacia', 'root', 'admin', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-});
+require('dotenv').config();
+
+const db = new Sequelize (
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'process.env.BD_HOST',
+    dialect: 'mysql',
+    logging: false
+  }
+);
 db.authenticate()
   .then(() => {
     console.log('✅ Conexión a MySQL exitosa');
